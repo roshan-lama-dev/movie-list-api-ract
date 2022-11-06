002bc16ac6cdb19fa73b09c355868fa87a1dd027
Havochalo <officialmail85@gmail.com>
2022-11-06 13:48:30 +1100

    deployment

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Title = () => {
-  return <h2 className="mt-5 text-center mb-2">My movie list</h2>;
+  return <h2 className="mt-5 text-center mb-2">My Movie List</h2>;
 };
